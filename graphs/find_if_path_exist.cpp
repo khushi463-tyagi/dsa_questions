f0284094498ed320ca3c071622f54607aff15bad
khushi463-tyagi <tyagikhushi463@gmail.com>
2026-07-08 00:02:25 +0530

    class level implementation

--- graphs/find_if_path_exist.cpp
+++ graphs/find_if_path_exist.cpp
@@ -1,46 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int e;
-    cout<<"enter no. of edges";
-    cin>>e;
-    int n;
-    cout<<"enter no. of nodes";
-    cin>>n;
-    vector<vector<int>>adj(n);
-    for(int i=0;i<e;i++){
-        int x,y;
-        cin>>x>>y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
+class Solution {
+public:
+    bool pathExists(int n, vector<vector<int>> &adj, int source, int destination) {
 
-    }
-    queue<int>q;
-    int source;
-    int destination;
-    cout<<"enter source and destination";
-    cin>>source>>destination;
-    vector<int>vis(n,0);
-    vis[source]=1;
-    q.push(source);
-    while(!q.empty()){
-        int f=q.front();
-        q.pop();
-        for(auto i:adj[f]){
-            if(!vis[i]){
-                vis[i]=1;
-                q.push(i);
+        vector<int> vis(n, 0);
+        queue<int> q;
+
+        vis[source] = 1;
+        q.push(source);
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            if (node == destination)
+                return true;
+
+            for (int neigh : adj[node]) {
+                if (!vis[neigh]) {
+                    vis[neigh] = 1;
+                    q.push(neigh);
+                }
             }
-
         }
-    }
-    if(vis[destination]){
-        cout<<"path exist";
-    }
-    else{
-        cout<<"no path exist";
-    }
-return 0;
 
+        return false;
+    }
+};
+
+int main() {
+
+    int n, e;
+    cout << "Enter number of nodes and edges: ";
+    cin >> n >> e;
+
+    vector<vector<int>> adj(n);
+
+    cout << "Enter the edges:\n";
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);      
+    }
+
+    int source, destination;
+    cout << "Enter source and destination: ";
+    cin >> source >> destination;
+
+    Solution obj;
+
+    if (obj.pathExists(n, adj, source, destination))
+        cout << "Path Exists\n";
+    else
+        cout << "No Path Exists\n";
+
+    return 0;
 }
